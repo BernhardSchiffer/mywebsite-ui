f9b5b10a7fc439ade4d853babde7c91df377f9c9
Bernhard Schiffer <schiffer.be@gmail.com>
2020-04-12 17:24:18 +0200

    store user also in localstorage

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -6,24 +6,18 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: "root"
 })
 export class UserService {
-  private userSource = new BehaviorSubject<User>(null);
+  private userSource = new BehaviorSubject<User>(JSON.parse(localStorage.getItem("currentUser")));
   currentUser = this.userSource.asObservable();
 
   constructor() {}
 
-  /*
-  getUser(): User {
-    return JSON.parse(localStorage.getItem("currentUser"));
-  }
-  */
-
   saveUser(user: User) {
     this.userSource.next(user);
-    //localStorage.setItem("currentUser", JSON.stringify(user));
+    localStorage.setItem("currentUser", JSON.stringify(user));
   }
 
   deleteUser() {
     this.userSource.next(null);
-    //localStorage.removeItem("currentUser");
+    localStorage.removeItem("currentUser");
   }
 }
